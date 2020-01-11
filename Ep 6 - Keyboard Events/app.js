ae49ce43f77699119062e662e910338b6a2cee74
Coldersz <achmad.dzulfian7@gmail.com>
2020-01-11 11:12:28 +0700

    adding Keyboard Events folder

---
+++ Ep 6 - Keyboard Events/app.js
@@ -0,0 +1,16 @@
+new Vue ({
+    el: '#vue-app',
+    data: {
+        // cek: '#input'
+    },
+    methods: {
+        logName: function() {
+            this.cek 
+            console.log('You entered your name' )    
+        },
+        logAge: function() {
+            console.log('You entered your age')    
+            
+        }
+    }
+})
